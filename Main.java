edeb58abaa883915d1e66e1ef31aff56f026a5fd
Vaishnavi Mate <159476791+vaishnavimate@users.noreply.github.com>
2024-03-18 23:01:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.BrickBreakGame;
+
+import javax.swing.JFrame;
+
+
+class Main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		GamePlay gamePlay = new GamePlay();
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Brick Breaker");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+		
+	}
+}
+
